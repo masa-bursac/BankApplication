1bf61cb69e2f658ee024cf7075db88b2fb8ef733
masa.bursac <bursac.ra133.2018@uns.ac.rs>
2025-03-17 13:37:17 +0100

    setSafetyDepositBox implemented

--- BankApp/src/app/Savings.java
+++ BankApp/src/app/Savings.java
@@ -8,10 +8,20 @@ public class Savings extends Account {
 	public Savings(String name, String sSN, double initDeposit) {
 		super(name, sSN, initDeposit);
 		accountNumber = "1" + accountNumber;
+		setSafetyDepositBox();
 	}
 	
 	public void showInfo() {
 		super.showInfo();
-		System.out.println("ACCOUNT TYPE: Savings");
+		System.out.println(
+				"Your Savings Account Features" + 
+				"\n Safety Deposit Box ID: " + safetyDepositBoxID +
+				"\n Safety Deposit Box Key: " + safetyDepositBoxKey
+				);
+	}
+	
+	private void setSafetyDepositBox() {
+		safetyDepositBoxID = (int)(Math.random() * Math.pow(10, 3));
+		safetyDepositBoxKey = (int)(Math.random() * Math.pow(10, 4));
 	}
 }
