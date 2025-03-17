19cadaf4091e01fd75b8bd026cc09b0c0717b916
masa.bursac <bursac.ra133.2018@uns.ac.rs>
2025-03-17 14:58:48 +0100

    using data from csv file to make new accounts implemented

--- BankApp/src/app/BankAccountApp.java
+++ BankApp/src/app/BankAccountApp.java
@@ -1,20 +1,35 @@
 package app;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class BankAccountApp {
 
 	public static void main(String[] args) {
-		String file = "C:\\Users\\masa\\eclipse-workspace\\BankApplication\\BankApplication\\NewBankAccounts.csv";
-			
+		List<Account> accounts = new LinkedList<Account>();
+		
+		String file = "C:\\Users\\masa\\eclipse-workspace\\BankApplication\\BankApplication\\NewBankAccounts.csv";	
 		List<String[]> newAccountHolders = utilities.CSV.read(file);
 		
 		for (String[] accountHolder: newAccountHolders) {
-			System.out.println("NEW ACCOUNT");
-			System.out.println(accountHolder[0]);
-			System.out.println(accountHolder[1]);
-			System.out.println(accountHolder[2]);
-			System.out.println(accountHolder[3]);
+			String name = accountHolder[0];
+			String sSN = accountHolder[1];
+			String accountType = accountHolder[2];
+			double initDeposit = Double.parseDouble(accountHolder[3]);
+			if(accountType.equals("Savings")){
+				accounts.add(new Savings(name, sSN, initDeposit));
+			}
+			else if(accountType.equals("Checking")){
+				accounts.add(new Checking(name, sSN, initDeposit));
+			}
+			else {
+				System.out.println("ERROR READING ACCOUNT TYPE");
+			}
+		 }
+		
+		for (Account acc: accounts) {
+			acc.showInfo();
+			System.out.println("*****************");
 		}
 	}
 
